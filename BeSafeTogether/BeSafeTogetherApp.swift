4c3953f2d3b37036164e3f02084877f4b26fa098
Danial <danial-baizak@mail.ru>
2023-06-21 10:57:44 +0600

    Initial Commit

---
+++ BeSafeTogether/BeSafeTogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeSafeTogetherApp.swift
+//  BeSafeTogether
+//
+//  Created by Danial Baizak on 21.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct BeSafeTogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
